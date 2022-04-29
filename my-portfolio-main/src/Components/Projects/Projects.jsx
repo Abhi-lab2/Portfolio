df383a8e2ca9252d9b57018973f86b7b3ebd9117
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-04-29 22:12:53 +0530

    updated

---
+++ my-portfolio-main/src/Components/Projects/Projects.jsx
@@ -0,0 +1,307 @@
+import React from "react";
+import "./Projects.css";
+import { FaReact, FaHardHat } from "react-icons/fa";
+import {
+  SiBlockchaindotcom,
+  SiHtml5,
+  SiMaterialui,
+  SiChai,
+  SiEthereum,
+  SiExpress,
+  SiSocketdotio,
+  SiTailwindcss,
+  SiReactrouter,
+  SiJavascript,
+  SiMongodb,
+  SiNodedotjs,
+} from "react-icons/si";
+import { DiCss3 } from "react-icons/di";
+import { VscJson } from "react-icons/vsc";
+import { IoLogoJavascript } from "react-icons/io";
+export const Projects = () => {
+  return (
+    <>
+      <div className="section">
+        <h2 className="section__title different">Projects</h2>
+        <div className="allProjects">
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src="https://cdn-images-1.medium.com/max/800/1*z8M1Ap-l4VNue2_Qu4DQUw.png"
+                    alt="Pluralsight Clone"
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Pluralsight Clone</h2>
+                <p>
+                  Pluralsight, Inc. is an American privately held online
+                  education company that offers a variety of video training
+                  courses for software developers, IT administrators, and
+                  creative professionals through its website
+                </p>
+                <div>
+                  <SiNodedotjs />
+                  <SiExpress />
+                  <SiMongodb />
+                  <FaReact />
+                  <SiMaterialui />
+                </div>
+                <div>
+                  <a
+                    href="https://plurlsightclone.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/Abhi-lab2/Pluralsight_Clone"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src="https://cdn-images-1.medium.com/max/800/1*NMOLeAVdNPGOMBmYO0VzrQ.png"
+                    alt="Freshly Clone"
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Freshly Clone</h2>
+                <p>
+                  Freshly is a New York-based prepared meal delivery company
+                  that delivers throughout the United States. The Clone is build
+                  using HTML, CSS, JavaScript.
+                </p>
+                <div>
+                  <DiCss3 />
+                  <SiHtml5 />
+                  <IoLogoJavascript />
+                </div>
+                <div>
+                  <a
+                    href="https://freshly-new.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/Abhi-lab2/freshly-Clone"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src="https://cdn-images-1.medium.com/max/800/1*qU--eirYHvqb9BJB_lgHVA.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Movie App</h2>
+                <p>
+                  This is the Movie app build using api and shows all the movies
+                  with the ratings and the short description of the movie which
+                  is searched for This is a Sole Project and was build in a day.
+                </p>
+                <div>
+                  <DiCss3 />
+                  <SiHtml5 />
+                  <IoLogoJavascript />
+                </div>
+                <div>
+                  <a
+                    href="https://movies-reviewer-app.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/Abhi-lab2/The-Movie-App"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src="https://user-images.githubusercontent.com/66909138/164240744-5bc86679-e0b5-412b-9cae-1ee40e8aaf68.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Weather App</h2>
+                <p>
+                  Location based weather app. User can also search for their
+                  cities and can see weather data of respective places. As it
+                  Provides city weather details with inbuild Google-map - using
+                  weather API
+                </p>
+                <div>
+                  <DiCss3 />
+                  <SiHtml5 />
+                  <IoLogoJavascript />
+                </div>
+                <div>
+                  <a
+                    href="https://ab-weather.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/Abhi-lab2/Weather-App"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src="https://i.ibb.co/934WpdD/uniqlo-clone.png"
+                    alt="Uniqlo-Clone"
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>Uniqlo Clone</h2>
+                <p>
+                  UNIQLO is a clothing apparel company, from Japan. They also
+                  sell their products in online mode through their website. We
+                  cloned their website.
+                </p>
+                <div>
+                  <SiNodedotjs />
+                  <SiExpress />
+                  <SiMongodb />
+                  <SiHtml5 />
+                  <DiCss3 />
+                </div>
+                <div>
+                  <a
+                    href="https://uniqlo-clone.netlify.app/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/Shreyasgkhakal100/uniqlo-backend-2"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="projects_container" data-aos="fade-right">
+            <div className="project">
+              <div className="project_videocontainer">
+                <div>
+                  <img
+                    src="https://i.ibb.co/KVBGkdy/FFD69103-C304-42-E8-BCCD-8836-AAEFEA9-C.jpg"
+                    alt="PulsePlus-Clone"
+                  />
+                </div>
+              </div>
+              <div className="project_information">
+                <h2>PulsePlus Clone</h2>
+                <p>
+                  PulsePlus is an E-commerce web application for online Buying
+                  Medicines and Consult to Doctor and Book a near Testlab.
+                </p>
+                <div>
+                  <DiCss3 />
+                  <SiHtml5 />
+                  <IoLogoJavascript />
+                </div>
+                <div>
+                  <a
+                    href="https://pulseplus-clone.netlify.app/landing_page.html"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt onbt">
+                      See this Live
+                    </span>
+                  </a>
+                  <a
+                    href="https://github.com/Shreyasgkhakal100/www.pulseplus.in-Clone"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <span type="button" className="btns onbt">
+                      View Code
+                    </span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div> */}
+        </div>
+      </div>
+    </>
+  );
+};
